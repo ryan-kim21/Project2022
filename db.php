1a3c8a91e04dc58ff6c0a34ff5f8f515d111b51d
ryan-kim21 <63631441+ryan-kim21@users.noreply.github.com>
2022-12-27 16:41:40 +0900

    commit6

---
+++ db.php
@@ -0,0 +1,4 @@
+<?php
+$write = mysqli_connect("project2022-database01.cjq9hdnnrcyv.ap-northeast-2.rds.amazonaws.com","admin","admin900529!","pet");
+$read = mysqli_connect("project2022-database01.cjq9hdnnrcyv.ap-northeast-2.rds.amazonaws.com","admin","admin900529!","pet");
+?>
