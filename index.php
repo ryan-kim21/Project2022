3b2c6d8be2c218d67cc20941673d234e322fbfd1
ryan-kim21 <63631441+ryan-kim21@users.noreply.github.com>
2022-12-29 17:05:02 +0900

    commit19

--- index.php
+++ index.php
@@ -4,15 +4,8 @@
   <body>
 
   <h1>Login page</h1>
-  
-  <iframe
-        width="960"
-        height="720"
-        src="https://ap-northeast-2.quicksight.aws.amazon.com/sn/embed/share/accounts/648975347655/dashboards/754e45f0-475c-4116-8bb3-ec75614cc5cf?directory_alias=project2022-test">
-    </iframe>
-
-
 
  <button type="button" onclick="location.href='https://project2022.auth.ap-northeast-2.amazoncognito.com/signup?response_type=token&client_id=7u53bj0p0885gr2t7khj7fmmsk&redirect_uri=https://project2022-test.tk/rds.php' "> 로그인 </button>
+
 </body>
 </html>
