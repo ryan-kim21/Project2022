f826d75e84aaab40bd9f56d724fcc08cd7a0f8f2
ryan-kim21 <63631441+ryan-kim21@users.noreply.github.com>
2022-12-30 10:04:31 +0900

    commit22

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <body>
   <p><h1>Animal Hospital</h1></p>
   <p><h1>Login page</h1></p>
-   
+  <p><img src="https://i.picsum.photos/id/1062/600/600.jpg?hmac=vu0-ZdTVn6gnaZVTCT1XisRG1eL_YhlqNYOBV1nCxvk" width="100" height="200"></p>
   <p><button type="button" onclick="location.href='https://project2022.auth.ap-northeast-2.amazoncognito.com/signup?response_type=token&client_id=7u53bj0p0885gr2t7khj7fmmsk&redirect_uri=https://project2022-test.tk/rds.php' "> 로그인 </button></p>
 
 </body>
